1d76da2d8cfa9ca6d8238e744f0fd903ba37b6ae
Jonathan Moreira <jonathan.ariel.moreira@gmail.com>
2024-04-15 00:05:15 -0300

    Proyecto finalizado en rama de Optimización de codigo, el proximo paso sera realizar un merge hacia la rama Main

--- main.js
+++ main.js
@@ -1,66 +1,91 @@
 
- //productos Arrays
+///Función finalizar compra
 
- // Productos Hogar
- const productoshogar=[
-    {
-        nombre:"sillon",
-        categoria:"hogar",
-        precio:200000,
-    },
-    {
-        nombre:"colchon",
-        categoria:"hogar",
-        precio:100000,
-    },
-   {
-        nombre:"mesa",
-        categoria:"hogar",
-        precio:100000,
-    },
-    {
-        nombre:"estanteria",
-        categoria:"hogar",
-        precio:80000,
-    },
-    {
-        nombre:"escritorio",
-        categoria:"hogar",
-        precio:50000,
-    }
-  ]
-//  
+function finalizarCompra(){
+  comprafinalizada=prompt("El total a pagar de su compra es : $"+totalApagar+" Para finalizar la compra escriba OK").toLowerCase()
+  if (comprafinalizada==="ok"){
+    alert("Muchas gracias por su compra!!!")
+    carrito.length=0;
+    ventas()
+ }
+ else{
+  carrito.length=0;
+   ventas()
+ }
+}
 
- //Productos electrodomesticos
+///Función pago con descuento.
 
- const productoselectrodomesticos=[
+function pagoConDescuento (){
+  const pagoConDebito=total*descuento;
+  totalApagar=total-pagoConDebito;
+  console.log("Precio con descuento del 20% = $"+totalApagar);
+  finalizarCompra();
+}
 
-   {
-        nombre:"heladera",
-        categoria:"electrodomesticos",
-        precio:1200000
-    },
-   {
-        nombre:"cocina",
-        categoria:"electrodomesticos",
-        precio:400000
-    },
-    {
-        nombre:"microondas",
-        categoria:"electrodomesticos",
-        precio:300000
-    },
-    {
-        nombre:"aireacondicionado",
-        categoria:"electrodomesticos",
-        precio:1500000
-    },
-    {
-        nombre:"lavarropas",
-        categoria:"electrodomesticos",
-        precio:800000
-    }
- ]
+///Función pago con credito.
+
+function pagoConCredito (){
+  const cantidadDeCuotas=prompt("Elija la cantidad de cuotas en las que desea realizar la compra")
+           console.log("Cantidad de cuotas elegidas = "+Number(cantidadDeCuotas))
+           total=carrito.reduce((acumulado,producto)=>acumulado+producto.precio,0);
+          const totalDelRecargo=Number(cantidadDeCuotas)*credito;
+          console.log("El total del recargo con tarjeta de credito es de = %"+totalDelRecargo)
+          const totalConCredito=total*totalDelRecargo;
+          const totalMasRecargo=totalConCredito/100;
+          console.log("El valor en $ del total del recargo es de = $"+totalMasRecargo);
+          totalApagar=total+totalMasRecargo;
+          console.log("El total en $ a pagar más el recargo es de = $"+totalApagar);
+          totalXcuota=totalApagar/cantidadDeCuotas;
+          console.log("El valor en $ de cada cuota es de = $"+totalXcuota)
+          finalizarCompra();      
+          }
+
+
+// Funcion constructora
+
+class Producto {
+
+  constructor(nombre, categoria, precio) {
+ 
+   this.nombre = nombre;
+ 
+   this.categoria = categoria;
+ 
+   this.precio = precio;
+ 
+  }
+ 
+} 
+ 
+///Objetos creados con función constructora.
+
+///Objetos de Hogar.
+
+ const Sillon=new Producto("Sillon","Hogar",200000);
+ const Colchon=new Producto("Colchon","Hogar",100000);
+ const Mesa=new Producto("Mesa","Hogar",100000);
+ const Estanteria=new Producto("Estanteria","Hogar",80000);
+ const Escritorio=new Producto("Escritorio","Hogar",50000);
+
+///Objetos de Electrodomesticos.
+
+ const Heladera=new Producto("Heladera","Electrodomesticos",1200000);
+ const Cocina=new Producto("Cocina","Electrodomesticos",400000);
+ const Microondas=new Producto("Microondas","Electrodomesticos",300000);
+ const AireAcondicionado=new Producto("AireAcondicionado","Electrodomesticos",1500000);
+ const Lavarropas=new Producto("Lavarropas","Electrodomesticos",800000);
+
+ //Arrays de Productos para el Hogar
+
+const productoshogar=[Sillon,Colchon,Mesa,Estanteria,Escritorio];
+
+//Array de productos Electrodomesticos
+
+const productoselectrodomesticos=[Heladera,Cocina,Microondas,AireAcondicionado,Lavarropas];
+
+//carrito
+
  const carrito=[]
  //variables y constantes
 
@@ -128,52 +153,16 @@ do{
           do{
           formadepago=prompt("¿De que manera desea pagar?. Si desea pagar con debito se le realizara un descuento del 20% del total, en cambio si desea pagar con tarjeta de credito tendra un recargo de 7% por cuota hasta un maximo de 12 cuotas. Escriba D para DEBITO y C para CREDITO").toLowerCase()
           if(formadepago==="d"){
-            console.log("precio sin descuento = $"+total)
-           const pagoConDebito=total*descuento;
-            totalApagar=total-pagoConDebito;
-            console.log("Precio con descuento del 20% = $"+totalApagar)
-            comprafinalizada=prompt("El total a pagar de su compra es : $"+totalApagar+" Para finalizar la compra escriba OK").toLowerCase()
-            if (comprafinalizada==="ok"){
-              alert("Muchas gracias por su compra!!!")
-              carrito.length=0;
-              ventas()
-           }
-           else{
-            carrito.length=0;
-             ventas()
-           }
+            pagoConDescuento();
           }
           if(formadepago==="c"){
-           const cantidadDeCuotas=prompt("Elija la cantidad de cuotas en las que desea realizar la compra")
-           console.log("Cantidad de cuotas elegidas = "+Number(cantidadDeCuotas))
-           total=carrito.reduce((acumulado,producto)=>acumulado+producto.precio,0);
-          const totalDelRecargo=Number(cantidadDeCuotas)*credito;
-          console.log("El total del recargo con tarjeta de credito es de = %"+totalDelRecargo)
-          const totalConCredito=total*totalDelRecargo;
-          const totalMasRecargo=totalConCredito/100;
-          console.log("El valor en $ del total del recargo es de = $"+totalMasRecargo);
-          totalApagar=total+totalMasRecargo;
-          console.log("El total en $ a pagar más el recargo es de = $"+totalApagar);
-          totalXcuota=totalApagar/cantidadDeCuotas;
-          console.log("El valor en $ de cada cuota es de = $"+totalXcuota)
-          comprafinalizada=prompt("El total a pagar de su compra es : $"+totalApagar+" Para finalizar la compra escriba OK").toLowerCase()
-            if (comprafinalizada==="ok"){
-               alert("Muchas gracias por su compra!!!")
-               carrito.length=0;
-               ventas()
-            }
-            else{
-              carrito.length=0;
-              ventas()
-            }
-          
-          }
+           pagoConCredito();
         }
       
-          while(formadepago!=="d"||formadepago!=="c"||formadepago!==""||Number(formadepago))
+         
       }
-    
-
+      while(formadepago!=="d"||formadepago!=="c"||formadepago!==""||Number(formadepago))
+    }
 
 ///A partir de aqui el proceso es el mismo, solo que ahora se utiliza el segundo Array.
 
@@ -208,52 +197,19 @@ if (tipodeproducto==="e") {
         do{
         formadepago=prompt("¿De que manera desea pagar?. Si desea pagar con debito se le realizara un descuento del 20% del total, en cambio si desea pagar con tarjeta de credito tendra un recargo de 7% por cuota hasta un maximo de 12 cuotas. Escriba D para DEBITO y C para CREDITO").toLowerCase()
         if(formadepago==="d"){
-          console.log("precio sin descuento = $"+total)
-         const pagoConDebito=total*descuento;
-          totalApagar=total-pagoConDebito;
-          console.log("Precio con descuento del 20% = $"+totalApagar)
-          comprafinalizada=prompt("El total a pagar de su compra es : $"+totalApagar+" Para finalizar la compra escriba OK").toLowerCase()
-          if (comprafinalizada==="ok"){
-            alert("Muchas gracias por su compra!!!")
-            carrito.length=0;
-            ventas()
-         }
-         else{
-          carrito.length=0;
-           ventas()
-         }
+          pagoConDescuento();
         }
         if(formadepago==="c"){
-         const cantidadDeCuotas=prompt("Elija la cantidad de cuotas en las que desea realizar la compra")
-         console.log("Cantidad de cuotas elegidas = "+Number(cantidadDeCuotas))
-         total=carrito.reduce((acumulado,producto)=>acumulado+producto.precio,0);
-        const totalDelRecargo=Number(cantidadDeCuotas)*credito;
-        console.log("El total del recargo con tarjeta de credito es de = %"+totalDelRecargo)
-        const totalConCredito=total*totalDelRecargo;
-        const totalMasRecargo=totalConCredito/100;
-        console.log("El valor en $ del total del recargo es de = $"+totalMasRecargo);
-        totalApagar=total+totalMasRecargo;
-        console.log("El total en $ a pagar más el recargo es de = $"+totalApagar);
-        totalXcuota=totalApagar/cantidadDeCuotas;
-        console.log("El valor en $ de cada cuota es de = $"+totalXcuota)
-        comprafinalizada=prompt("El total a pagar de su compra es : $"+totalApagar+" Para finalizar la compra escriba OK").toLowerCase()
-          if (comprafinalizada==="ok"){
-             alert("Muchas gracias por su compra!!!")
-             carrito.length=0;
-             ventas()
-          }
-          else{
-            carrito.length=0;
-            ventas()
-          }
+          pagoConCredito();
         }
       } 
       while(formadepago!=="d"||formadepago!=="c" ||formadepago!==""||Number(formadepago))  
     }
-  }
-      while(tipodeproducto!=="h"||tipodeproducto!=="e"||Number(tipodeproducto) || tipodeproducto!=="")
+  }while(tipodeproducto!=="h"||tipodeproducto!=="e"||Number(tipodeproducto) || tipodeproducto!=="")
+} 
+
+     
       
     
- }
-
+ 
 ventas()
